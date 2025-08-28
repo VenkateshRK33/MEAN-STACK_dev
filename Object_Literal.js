09460b0bc33a8ff2b44aef1e5ff0d6c10b83595b
VenkateshRK33 <venkateshrkulkarni1@gmail.com>
2025-08-28 19:58:27 +0530

    .

---
+++ Object_Literal.js
@@ -0,0 +1,10 @@
+var facebook={
+    name:"facbook",
+    ceo:{
+        fname:"mark",
+        favcolor:"red"
+    },
+    "stock off company":110
+};
+console.log(facebook.ceo.fname)
+console.log(facebook["stock off company"])
